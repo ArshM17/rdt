6a29a61b11335ef7b383cb2b03711734740a38e5
ArshM17 <arshmaknojia5@gmail.com>
2023-10-24 15:01:27 +0530

    finish rdt 1.0

--- rdt1.0/Client.java
+++ rdt1.0/Client.java
@@ -1,43 +1,29 @@
-import java.io.*;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Client {
-
-	private Socket clientSocket = null;
-	private InputStream in = null;
-	private OutputStream out = null;
-	Client(String serverIP, int serverPort){
-		try{
-    	    		clientSocket = new Socket(serverIP, serverPort);
-			System.out.println("Connected");
-	    	    	Scanner scan = new Scanner(System.in);
-    	    		while(true){
-    	    			System.out.print("Enter a message to be sent to the server:(Q to quit)");
-    		    		String msg = scan.nextLine();
-    		    		out = clientSocket.getOutputStream();
-    		    		out.write(msg.getBytes());
-				if(msg.equals("q") || msg.equals("Q")) break;
-				in = clientSocket.getInputStream();
-				byte buffer[] = new byte[1024];
-				int serverMsg = in.read(buffer);
-				System.out.println("Server says: "+new String(buffer, 0, serverMsg));
-			}
-    		    	scan.close();
-    	    		clientSocket.close();
-		} catch(Exception e){
-			System.out.println("Some error occured: "+e);
-		}
-    	}
-	
-	public static void main(String[] args){
-		if(args.length<=1){
-    	    		System.out.println("Too few command line arguments, this command takes 2 parameters. 1st: IP address of server, 2nd: Server Port");
-    	    		return;
-    	    	}
-    	    	String serverIP = args[0];
-    	    	int serverPort = Integer.valueOf(args[1]);
-		Client client = new Client(serverIP, serverPort);
-	}
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+	    Scanner scan = new Scanner(System.in);
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            int serverPort = 12345;
+	    while(true){
+            	System.out.print("Enter a message to be sent to the server:(Q to quit)");
+            	String message = scan.nextLine();
+            	if(message.equals("Q") || message.equals("q")) break;
+            	byte[] sendData = message.getBytes();
+            	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+            	socket.send(sendPacket);
+            	System.out.println("Sent: " + message);
+	    }
+	    scan.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
